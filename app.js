6fa0082f306bfb6b70ceb20655a07549ea48c07a
japhlett <jatakyiwaa001@st.ug.edu.gh>
2024-09-18 15:14:36 +0000

    read and write to files asynchronously using callback help

--- app.js
+++ app.js
@@ -21,12 +21,32 @@ rl.on("close", ()=>{
 */
 
 /*
-Reading and write to a file
+Reading and write to a file synchronously
 */
 
-let textIn = fs.readFileSync('./Files/input.txt', 'utf-8')
+/*let textIn = fs.readFileSync('./Files/input.txt', 'utf-8')
 console.log(textIn)
 
 let content = `Data read from input.txt: ${textIn}. \nDate Created: ${new Date()}`
 
-fs.writeFileSync('./Files/output.txt', content)
+fs.writeFileSync('./Files/output.txt', content)*/
+
+
+/*
+Reading and write to a file asynchronously
+*/
+
+fs.readFile('./Files/start.txt', 'utf-8',(err, data) =>{
+    console.log(data)
+    fs.readFile(`./Files/${data}.txt`,'utf-8',(err1, data1) => {
+        console.log(data1)
+        fs.readFile('./Files/append.txt', 'utf-8', (err2, data2) =>{
+            console.log(data2)
+            fs.writeFile('Files/output.txt', `${data1}\n\n ${data2}\n\n ${new Date()}`, (err3, data3) =>{
+                console.log('File written successfully')
+            })
+        })
+    })
+})
+
+console.log('Reading file....')
