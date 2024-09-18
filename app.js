dc5932c0891d05ff01681e8883298f01c3272673
japhlett <jatakyiwaa001@st.ug.edu.gh>
2024-09-18 14:13:45 +0000

    read and write files synchronously in node.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import readline from "readline";
+import fs from "fs";
+import { text } from "express";
 
+/*
+Reading input and writing output
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -14,3 +18,15 @@ rl.on("close", ()=>{
     console.log("Interface closed");
     process.exit(0);
 })
+*/
+
+/*
+Reading and write to a file
+*/
+
+let textIn = fs.readFileSync('./Files/input.txt', 'utf-8')
+console.log(textIn)
+
+let content = `Data read from input.txt: ${textIn}. \nDate Created: ${new Date()}`
+
+fs.writeFileSync('./Files/output.txt', content)
